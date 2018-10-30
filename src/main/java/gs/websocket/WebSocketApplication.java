6ebbe6d9c6771ebcf1492f760bfd2fa51919275e
Leon Garibello <lgaribelo@hotmail.com>
2018-10-30 00:52:58 -0500

    Add first version of getting started with websockets

---
+++ src/main/java/gs/websocket/WebSocketApplication.java
@@ -0,0 +1,14 @@
+package gs.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class WebSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketApplication.class, args);
+    }
+}
---
+++ src/main/java/gs/websocket/model/Message.java
@@ -0,0 +1,14 @@
+package gs.websocket.model;
+
+public class Message {
+
+    private String from;
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+}
